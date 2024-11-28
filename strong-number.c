59a411188afd849b2b4fc3ee2bfc1514db18612c
PPProsenjite <prosenjitpappu03455@gmial.com>
2024-11-29 00:07:26 +0600

    Strong number in c

---
+++ strong-number.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int checkStrongNumber(int* n){
+    int i, sum = 0;
+    while( *n > 0){
+        int fact = 1;
+        for(i = 1; i <= *n % 10; i++){
+            fact = fact * i ;
+        }
+        sum = sum + fact;
+        *n = *n / 10;
+    }
+    
+    return sum;
+    
+}
+int main() {
+    int num ;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    int *ptr = &num;
+    if(*ptr == checkStrongNumber(ptr)){
+        printf("%d number is strong number.",ptr);
+    }
+    else 
+    printf("This number is not a strong number.");
+
+    return 0;
+}
